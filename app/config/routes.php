2296939b9f3cbb5afdd58ea1325c5907e588fd45
Malardier <pierre.malardier@laplateforme.io>
2021-05-26 13:23:03 +0200

    root connexion

--- app/config/routes.php
+++ app/config/routes.php
@@ -5,15 +5,8 @@ use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/', function (
-        ServerRequestInterface $request,
-        ResponseInterface $response
-    ) {
-        $response->getBody()->write('Hello World!');
-        return $response;
-    });
 
-    $app->get('/connexion', \App\Action\Connexion::class);
+    $app->get('/', \App\Action\Connexion::class);
 
     $app->post('/googleAuth', \App\Action\UserGoogleAuth::class);
 
