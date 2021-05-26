9805db3dddf56ad4291c573569b5bf3e04422e93
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-05-26 10:00:54 +0200

    Google auth handle in js side

--- app/public/js/connexion.js
+++ app/public/js/connexion.js
@@ -1,23 +1,17 @@
 const pathMain = $('#pathMain').val();
-console.log(pathMain);
 
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('Prodilfe', profile)
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-
     let auth = googleUser.getAuthResponse();
     let token = auth.id_token;
 
-    let form = new FormData();
-    form.append("id_token", token)
-
-    fetch(pathMain + "/googleAuth", {
-        method: "POST",
-        body: form
+    $.ajax({
+        url: pathMain + "/googleAuth",
+        type: "POST",
+        data: {id_token: token}
+    }).done(()=>{
+        console.log("Success")
+    }).fail(()=>{
+        console.log("Fail")
     })
 
 }
