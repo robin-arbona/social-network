5e48f1814be5b59f1af09f192ef0acce1b73c678
Malardier <pierre.malardier@laplateforme.io>
2021-05-31 12:55:23 +0200

    button deco in navbar with redirection

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -28,8 +28,9 @@ function onSignIn(googleUser) {
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
+        auth2.disconnect();
         console.log('User signed out.');
-        location.reload()
+        window.location = pathMain + "/";
     });
 }
 
