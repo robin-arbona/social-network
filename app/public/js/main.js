32764a855334ef1d6537e60acb1d7adc164529c8
Malardier <pierre.malardier@laplateforme.io>
2021-05-31 10:31:06 +0200

    button ok need to modify pathMain

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,2 +1,28 @@
 console.log("Main lié")
-io("social.network:3001");
+//io("social.network:3001");
+
+const pathMain = $('#pathMain').val();
+
+function onSignIn(googleUser) {
+    let auth = googleUser.getAuthResponse();
+    let token = auth.id_token;
+
+    $.ajax({
+        url: pathMain + "/googleAuth",
+        type: "POST",
+        data: { id_token: token }
+    }).done(() => {
+        window.location = pathMain + "/wall";
+    }).fail(() => {
+        console.log("Fail");
+    })
+    console.log(window.location);
+}
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+
+    });
+}
+
