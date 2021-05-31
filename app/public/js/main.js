71b795f43b798ec74b66ac5d92e63318024245e2
Malardier <pierre.malardier@laplateforme.io>
2021-05-31 11:37:41 +0200

    deco button done

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -2,6 +2,8 @@ console.log("Main lié")
 //io("social.network:3001");
 
 const pathMain = $('#pathMain').val();
+console.log(window.location.href.slice(-1));
+
 
 function onSignIn(googleUser) {
     let auth = googleUser.getAuthResponse();
@@ -12,17 +14,22 @@ function onSignIn(googleUser) {
         type: "POST",
         data: { id_token: token }
     }).done(() => {
-        window.location = pathMain + "/wall";
+        if (window.location.href.slice(-1) == '/') {
+            window.location = pathMain + "/wall";
+        } else {
+
+        }
+
     }).fail(() => {
         console.log("Fail");
     })
-    console.log(window.location);
+
 }
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
-
+        location.reload()
     });
 }
 
