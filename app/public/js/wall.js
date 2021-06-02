726766c82142c082d39a86531b9657e9f5328bcf
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-06-02 09:34:13 +0200

    Formating wall js in POO

--- app/public/js/wall.js
+++ app/public/js/wall.js
@@ -1,26 +1,45 @@
 let urlParsed = window.location.pathname.split('/')
 
-let userWall = urlParsed[urlParsed.indexOf('wall')+1] != undefined ? urlParsed[urlParsed.indexOf('wall')+1] : '';
+let userWall = urlParsed[urlParsed.indexOf('wall')+1] != undefined ? urlParsed[urlParsed.indexOf('wall')+1] : null;
 
 let path = document.querySelector('#pathMain').getAttribute('value')
 
 document.addEventListener('DOMContentLoaded',()=>{
-    loadPosts(userWall,1)
+    let pageLoader = new PageLoader(path+'/post',userWall)
 })
 
-const loadPosts = async (userId,page = 1)=>{
-    let url = path + '/post/' + page;
-    url = userId > 0 ? url + '/' + userId : url;
-    let json = await fetch(url)
-        .then(reponse=>reponse.json())
-        .then(json=>json)
-    json.posts.forEach(post => {
-        let postElement = document.createElement("div");
-        postElement.innerHTML = formatePost(post);
-        document.querySelector(".post").appendChild(postElement);
-    });
-}
 
-const formatePost = (post) => {
-    return `<h1>${post.post_name}</h1><p>${post.post_content}</p><hr>`
+class PageLoader {
+    constructor(APIEntryPoint,param = null){
+        this.entryPoint = APIEntryPoint;
+        this.currentPage = 1;
+        this.init();
+        this.param = param;
+    }
+    getUrl(){
+        let url = this.entryPoint + '/' + this.currentPage;
+        return this.param == null ? url : url  +'/' + this.param;
+    }
+    async init(){
+        let results = await this.fetchJson(this.getUrl());
+        this.totalPage = results.totalPage;
+        this.displayResults(results.posts);
+    }
+    async fetchJson(url){
+        let json = await fetch(url)
+        .then(reponse=>reponse.json())
+        .then(json=>json);
+        return json;
+    }
+    displayResults(results){
+        results.forEach(result => {
+            let postElement = document.createElement("div");
+            postElement.innerHTML = this.formatePost(result);
+            document.querySelector(".post").appendChild(postElement);
+        });
+    }
+    formatePost(post){
+        return `<h1>${post.post_name}</h1><p>${post.post_content}</p><hr>`
+    }
+    
 }
